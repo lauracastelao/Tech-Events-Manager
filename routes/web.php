6d786dbda02693697e783e2c05a0f1b50b73a68d
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-08-24 12:51:41 +0200

    log en db

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
