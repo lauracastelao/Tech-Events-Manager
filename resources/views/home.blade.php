20293dac673c02372c1782dec7759f787ffd855b
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-08-25 17:13:28 +0200

    nada

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,12 +21,12 @@
         @if (Route::has('login'))
             <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                 @auth
-                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    <a href="{{ url('/dashboard') }}" class="text-ml text-gray-700 dark:text-gray-500 ">Dashboard</a>
                 @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                    <a href="{{ route('login') }}" class="text-ml text-white-700 dark:text-gray-500 ">Log in</a>
 
                     @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-white-700 dark:text--500 ">Register</a>
                     @endif
                 @endauth
             </div>
