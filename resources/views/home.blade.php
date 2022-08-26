538c6044e226f92f0354b4cd3461768cbfe11506
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-08-26 09:35:19 +0200

    cambiando a vivi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,12 +10,12 @@
     <link rel="stylesheet" 
     href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css">
   </head>
-  <body class="bg-indigo-200">
+  <body class="bg-gradient-to-r from-indigo-300 via-purple-500 to-pink-300">
     <title>@yield('title')Friki Calendar</title>
 
     <nav class="flex py-5 bg-purple-500 text-white">
       <div class="w-1/2 px-12 mr-auto flex">
-        <p class="text-2xl font-bold ">Friki Calendar</p>
+        <p class="text-2xl font-bold font-sans">Friki Calendar</p>
         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="cat" class="ml-4 mt-2 w-4 h-4" mt-2role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
         <path fill="currentColor" d="M290.59 192c-20.18 0-106.82 1.98-162.59 85.95V192c0-52.94-43.06-96-96-96-17.67 0-32 14.33-32 32s14.33 32 32 32c17.64 0 32 14.36 32 32v256c0 35.3 28.7 64 64 64h176c8.84 0 16-7.16 16-16v-16c0-17.67-14.33-32-32-32h-32l128-96v144c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V289.86c-10.29 2.67-20.89 4.54-32 4.54-61.81 0-113.52-44.05-125.41-102.4zM448 96h-64l-64-64v134.4c0 53.02 42.98 96 96 96s96-42.98 96-96V32l-64 64zm-72 80c-8.84 0-16-7.16-16-16s7.16-16 16-16 16 7.16 16 16-7.16 16-16 16zm80 0c-8.84 0-16-7.16-16-16s7.16-16 16-16 16 7.16 16 16-7.16 16-16 16z"></path>
     
@@ -111,18 +111,18 @@
     <div class="lg:w-1/6 m-2 justify-center">
         <a href="#">
             <div class="flex relative justify-center">
-                <img alt="gallery" class="absolute inset-0 w-full h-full object-fit object-center rounded" src=" 
+                <img alt="gallery" class="absolute inset-0 w-full h-full object-fit object-center rounded shadow-lg" src=" 
                 https://img.freepik.com/psd-gratis/plantilla-poster-dia-orgullo-friki_23-2148954984.jpg?w=2000" alt=""/>
                 
                 <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-300 bg-indigo-400 opacity-0 hover:opacity-100 justify-center">
                     <h1 class="tracking-widest text-mg title-font font-bold text-black mb-1">Cosplay</h1>
                     <h2 class="title-font text-mg font-bold text-black hover:text-indigo-100 mb-3">06/02/2023</h2>
                     <p class="leading-relaxed text-black">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                    <button type="button" class="font-small uppercase rounded shadow-md 
-                    hover:bg-purple-500
-                      hover:shadow-lg focus:bg-white focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 "> ➕ 
-                    Apuntarse 
-                      </button>
+                <button type="button" class="font-small uppercase rounded shadow-md 
+                hover:bg-purple-500
+                  hover:shadow-lg p-2 focus:bg-white focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 "> ➕ 
+                Apuntarse 
+                  </button>
                 </div>
             </div>
         </a>
@@ -130,7 +130,7 @@
       <div class="lg:w-1/6 m-2 justify-center">
         <a href="#">
             <div class="flex relative justify-center">
-                <img alt="gallery" class="absolute inset-0 w-full h-full object-fit object-center rounded" src=" 
+                <img alt="gallery" class="absolute inset-0 w-full h-full object-fit object-center rounded shadow-lg " src=" 
                 https://cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/FFTXTJ2GOZCQHDTG4UD3E6OXVY.jpg" alt=""/>
                 
                 <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-300 bg-indigo-400 opacity-0 hover:opacity-100 justify-center">
@@ -139,7 +139,7 @@
                     <p class="leading-relaxed text-black">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
                     <button type="button" class="font-small uppercase rounded shadow-md 
                     hover:bg-purple-500
-                      hover:shadow-lg focus:bg-white focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 "> ➕ 
+                      hover:shadow-lg p-2 focus:bg-white focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 "> ➕ 
                     Apuntarse 
                       </button>
                 </div>
@@ -149,7 +149,7 @@
       <div class="lg:w-1/6 m-2 justify-center">
         <a href="#">
             <div class="flex relative justify-center">
-                <img alt="gallery" class="absolute inset-0 w-full h-full object-fit object-center rounded" src=" 
+                <img alt="gallery" class="absolute inset-0 w-full h-full object-fit object-center rounded shadow-lg" src=" 
                 https://www.guiadecadiz.com/sites/default/files/2022/AGENDA/friki/Cartel%20II%20Salon%20de%20Manga.jpg" alt=""/>
                 
                 <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-300 bg-indigo-400 opacity-0 hover:opacity-100 justify-center">
@@ -158,7 +158,7 @@
                     <p class="leading-relaxed text-black">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
                     <button type="button" class="font-small uppercase rounded shadow-md 
                     hover:bg-purple-500
-                      hover:shadow-lg focus:bg-white focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 "> ➕ 
+                      hover:shadow-lg p-2 focus:bg-white focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 "> ➕ 
                     Apuntarse 
                       </button>
                 </div>
@@ -168,8 +168,8 @@
       <div class="lg:w-1/6 m-2 justify-center">
         <a href="#">
             <div class="flex relative justify-center">
-                <img alt="gallery" class="absolute inset-0 w-full h-full object-fit object-center rounded" src=" 
-                 https://th.bing.com/th/id/OIP.PMmw7ScrZTqd1tX_tW3jqwAAAA?pid=ImgDet&rs=1" alt=""/>
+                <img alt="gallery" class="absolute inset-0 w-full h-full object-fit object-center rounded shadow-lg" src=" 
+                https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLgzk1RIAKuqL-kQgiCZBoyzL8EFFhj7HCwA&usqp=CAU" alt=""/>
                 
                 <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-300 bg-indigo-400 opacity-0 hover:opacity-100 justify-center">
                     <h1 class="tracking-widest text-mg title-font font-bold text-black mb-1">Cosplay</h1>
@@ -177,7 +177,7 @@
                     <p class="leading-relaxed text-black">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
                     <button type="button" class="font-small uppercase rounded shadow-md 
                     hover:bg-purple-500
-                      hover:shadow-lg focus:bg-white focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 "> ➕ 
+                      hover:shadow-lg p-2 focus:bg-white focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 "> ➕ 
                     Apuntarse 
                       </button>
                 </div>
