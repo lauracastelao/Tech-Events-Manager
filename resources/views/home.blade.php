0ed60b4f294fe0f121bc1fc83c10c21fac254d0b
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-08-26 14:44:08 +0200

    n

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -16,8 +15,12 @@
     <nav class="flex py-5 bg-purple-500 text-white">
       <div class="w-1/2 px-12 mr-auto flex">
         <p class="text-2xl font-bold font-sans">Friki Calendar</p>
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="cat" class="ml-4 mt-2 w-4 h-4" mt-2role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-        <path fill="currentColor" d="M290.59 192c-20.18 0-106.82 1.98-162.59 85.95V192c0-52.94-43.06-96-96-96-17.67 0-32 14.33-32 32s14.33 32 32 32c17.64 0 32 14.36 32 32v256c0 35.3 28.7 64 64 64h176c8.84 0 16-7.16 16-16v-16c0-17.67-14.33-32-32-32h-32l128-96v144c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V289.86c-10.29 2.67-20.89 4.54-32 4.54-61.81 0-113.52-44.05-125.41-102.4zM448 96h-64l-64-64v134.4c0 53.02 42.98 96 96 96s96-42.98 96-96V32l-64 64zm-72 80c-8.84 0-16-7.16-16-16s7.16-16 16-16 16 7.16 16 16-7.16 16-16 16zm80 0c-8.84 0-16-7.16-16-16s7.16-16 16-16 16 7.16 16 16-7.16 16-16 16z"></path>
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="cat" class="ml-4 mt-2 w-4 h-4" mt-2role="img" xmlns="http://www.w3.org/2000/svg" 
+viewBox="0 0 512 512">
+        <path fill="currentColor" d="M290.59 192c-20.18 0-106.82 1.98-162.59 85.95V192c0-52.94-43.06-96-96-96-17.67 0-32 14.33-32 32s14.33 32 32 32c17.64 0 32 14.
+32 32v256c0 35.3 28.7 64 64 64h176c8.84 0 16-7.16 16-16v-16c0-17.67-14.33-32-32-32h-32l128-96v144c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V289.86c-10.
+2.67-20.89 4.54-32 4.54-61.81 0-113.52-44.05-125.41-102.4zM448 96h-64l-64-64v134.4c0 53.02 42.98 96 96 96s96-42.98 96-96V32l-64 64zm-72 80c-8.84 0-16-7.
+16-16-16s7.16-16 16-16 16 7.16 16 16-7.16 16-16 16zm80 0c-8.84 0-16-7.16-16-16s7.16-16 16-16 16 7.16 16 16-7.16 16-16 16z"></path>
     
         @if (Route::has('login'))
         <div class="hidden fixed top-2 right-0 px-6 py-4 sm:block">
@@ -56,7 +59,8 @@
 
     <button @click="back()"
         class="absolute left-14 top-1/2 -translate-y-1/2 w-11 h-11 flex justify-center items-center rounded-full shadow-md z-10 bg-gray-100 hover:bg-gray-200">
-        <svg class=" w-8 h-8 font-bold transition duration-500 ease-in-out transform motion-reduce:transform-none text-gray-500 hover:text-gray-600 hover:-translate-x-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+        <svg class=" w-8 h-8 font-bold transition duration-500 ease-in-out transform motion-reduce:transform-none text-gray-500 hover:text-gray-600 
+hover:-translate-x-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M15 19l-7-7 7-7">
             </path>
@@ -65,17 +69,32 @@
 
     <button @click="next()"
     class="absolute right-14 top-1/2 translate-y-1/2 w-12 h-11 lex justify-center items-center rounded-full shadow-md z-10 bg-gray-100 hover:bg-gray-500">
-        <svg class=" w-8 h-8 font-bold transition duration-500 ease-in-out transform motion-reduce:transform-none text-gray-500 hover:text-gray-600 hover:translate-x-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+        <svg class=" w-8 h-8 font-bold transition duration-500 ease-in-out transform motion-reduce:transform-none text-gray-500 hover:text-gray-600 
+hover:translate-x-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M9 5l7 7-7 7"></path>
         </svg>
     </button>
 </article>
 
+{{-- <x-card> --}}
+{{-- </x-card> --}}
+{{-- <x-card> --}}
+{{-- </x-card> --}}
+{{-- <x-card> --}}
+{{-- </x-card> --}}
+{{-- <x-card> --}}
+{{-- </x-card> --}}
+
+<!-- component -->
+
+
 
 <div class="flex flex-wrap justify-center">
     <a href="#">
         <div class="lg:w-1/6 m-2">
+        
+        
         
           <div class="flex relative justify-center">
               <img alt="gallery" class="absolute inset-0 w-full h-full object-fit object-center rounded" src=" 
@@ -170,42 +189,36 @@
             </div>
         </a>
       </div>
-  
-      <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    @livewire('product-component')
-                   
-                </div>
-            </div>
-        </div>
-    </div>
+      @livewire('product-component')
   </section>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
-
-
-
-
-
-
-
-
+<script>
+    document.addEventListener('alpine:init', () => {
+        Alpine.data('slider', () => ({
+            currentIndex: 1,
+            images: [
+                'https://github.com/lauracastelao/image/blob/main/comic.jpg?raw=true',
+                'https://raw.githubusercontent.com/lauracastelao/image/main/Mi%20proyecto.jpg',
+                'https://github.com/lauracastelao/image/blob/main/comic.jpg?raw=true',
+                'https://github.com/lauracastelao/image/blob/main/santa.jpg?raw=true',
+                'https://raw.githubusercontent.com/lauracastelao/image/main/Mi%20proyecto.jpg'
+            ],
+            back() {
+                if (this.currentIndex > 1) {
+                    this.currentIndex = this.currentIndex - 1;
+                }
+            },
+            next() {
+                if (this.currentIndex < this.images.length) {
+                    this.currentIndex = this.currentIndex + 1;
+                } else if (this.currentIndex <= this.images.length){
+                    this.currentIndex = this.images.length - this.currentIndex + 1
+                }
+            },
+        }))
+    })
+</script>
         
   </body>
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -3,42 +3,8 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-
-                    <div class="row">
-                        <div class="col-md-8">
-                            <div class="mt-2 table-responsive-md">
-                                <table class="table table-striped">
-                                    <thead>
-                                      <tr>
-                                       
-                                        <th scope="col">Nombre</th>
-                                        <th scope="col">Descripción</th>
-                                        <th scope="col"></th>
-                                      </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($products as $product)
-                                            <tr>
-                                                
-                                                <td>{{$product->name}}</td>
-                                                <td>{{ $product->description }}<
-                                                <td>
-                                                    <button type="button" class=
-                                                </td>
-                                                <td>
-                                                    <button type="button" class=
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                                {{ $products->links()}}
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                        </div>
-                    </div>
-                    
+                    @livewire('product-component')
+                   
                 </div>
             </div>
         </div>
