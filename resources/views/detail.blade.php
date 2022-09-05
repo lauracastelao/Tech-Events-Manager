bb708eede5d84efaa7e583af1b6b74f022e0f1d2
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-09-05 11:36:11 +0200

    detail view

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,8 +1,21 @@
 @include('Layouts.layout')
 
-<div>
-    <ul>
-        <li>{{$product->title}}</li>
-        <li>{{$product->date}}</li>
-    </ul>
+<div class=container>
+    <div class=card>
+        <div class=image>
+            <img href="#" src="{{ $product->image }}" alt="" />
+        </div>
+        <div class=content>
+            <a href="product/{{ $product->id }}">
+                <h3>{{ $product->title }}</h3>
+            </a>
+            <p>{{ $product->date }}</p>
+            <p>{{ $product->description }}</p>
+
+        </div>
+
+        <button class="btn"><a href="#">Apuntarme</a></button></button>
+
+
+    </div>
 </div>
