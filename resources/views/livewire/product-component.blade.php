69226a4155fcf9763ffd7e927ecbf39cdb515b84
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-09-09 09:21:11 +0200

    botones 20

--- resources/views/livewire/product-component.blade.php
+++ resources/views/livewire/product-component.blade.php
@@ -23,7 +23,7 @@
             <form action="{{ route('products.edit', $product->id) }}" method="post">
                 @csrf
                 @method('GET')
-                <button class="inline-flex items-center  py-2 bg-indigo-400 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">Editar</button>
+                <button class="inline-flex items-center  py-2 bg-indigo-400 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150" >Editar</button>
             </form>
         </div>
         </div>
